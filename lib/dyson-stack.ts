29062ddfe23846f2b982cc14db2aad5d12908cd5
Kevin Stasko <kevin.stasko@gmail.com>
2022-01-30 16:31:24 -0600

    feat! - aws-hosted dyson deploy

--- lib/dyson-stack.ts
+++ lib/dyson-stack.ts
@@ -1,6 +1,8 @@
 import { Construct } from 'constructs';
 import {Stack, StackProps } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3'
+import { AmazonLinuxImage, Instance, InstanceClass, InstanceSize, InstanceType, Vpc }  from 'aws-cdk-lib/aws-ec2';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment'
 
 interface DysonProps extends StackProps {
   encryptBucket?: boolean;
@@ -10,6 +12,22 @@ export class DysonStack extends Stack {
   constructor(scope: Construct, id: string, props?: DysonProps) {
     super(scope, id, props);
 
-    const bucket = new Bucket(this, 'MyFirstBucket');
+    const bucket = new Bucket(this, 'dyson-bucket');
+
+    new BucketDeployment(this, 'DeployWebsite', {
+      sources: [Source.asset('./appcode')],
+      destinationBucket: bucket,
+    })
+
+    const vpc = new Vpc(this, 'dyson-vpc');
+
+    const instance = new Instance(this, 'dyson-ec2-instance', {
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MICRO),
+      machineImage: new AmazonLinuxImage(),
+      vpc: vpc
+    });
+
+    bucket.grantRead(instance)
+
   }
 }
